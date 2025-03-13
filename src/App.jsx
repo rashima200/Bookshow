c061c0ee3de3b1dc7b906d4db7f66616136c996a
rashima200 <rashimarasangi200@gmail.com>
2025-03-13 12:57:11 +0530

    chore:project setup configured

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-900 p-4">
+
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
